c78e8ddac3a22cb31a87398035240ce2642fa0ca
jmanzaneque <jmanzaneque10@gmail.com>
2018-03-08 10:17:30 +0100

    Añadido control registrar Editorial

--- SSDD/CatalogoLibreriaSSDD/src/main/java/catalogo/controller/EditorialController.java
+++ SSDD/CatalogoLibreriaSSDD/src/main/java/catalogo/controller/EditorialController.java
@@ -1,5 +1,34 @@
 package catalogo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import catalogo.model.Editorial;
+import catalogo.repository.EditorialRepositorio;
+
+@Controller
 public class EditorialController {
+	
+	@Autowired
+	private EditorialRepositorio repEditorial;
+	
+	@RequestMapping(value="/registroEditorialCompletado")
+	public String registraEditorial(
+			@RequestParam(value="nombre") String nombre,
+			@RequestParam(value="telefono") long telefono,
+			@RequestParam(value="email") String email,
+			@RequestParam(value="cPostal") long cPostal,
+			@RequestParam(value="nif") long nif,
+			Model model) {
+		
+		Editorial newEditorial = new Editorial(nombre, telefono, email, cPostal, nif);
+		repEditorial.save(newEditorial);
+		model.addAttribute("repEditoriales", repEditorial);
+		return "editorialRegistrada";
+	}
+	
 
 }
--- SSDD/CatalogoLibreriaSSDD/src/main/java/catalogo/model/Editorial.java
+++ SSDD/CatalogoLibreriaSSDD/src/main/java/catalogo/model/Editorial.java
@@ -1,17 +1,35 @@
 package catalogo.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Editorial {
 	@Id
-	private long nif;
-	
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
 	
 	private String nombre, email;
-	private long telefono, cPostal;
+	private long telefono, cPostal, nif;
 	
+	public Editorial (String nombre, long telefono, String email, long cPostal, long nif) {
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.email = email;
+		this.cPostal = cPostal;
+		this.nif = nif;
+	}
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
 	public long getNif() {
 		return nif;
 	}
