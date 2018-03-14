9732b5b4dc227141b72fe65f528cb9011401fa1b
steven121517 <steven121517@github.com>
2018-03-14 13:59:05 +0100

    Corregido error mostrar 8082/
    faltaba el controller en el libroRepositorio

--- SSDD/CatalogoLibreriaSSDD/src/main/java/catalogo/controller/LibroController.java
+++ SSDD/CatalogoLibreriaSSDD/src/main/java/catalogo/controller/LibroController.java
@@ -1,13 +1,14 @@
 package catalogo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import catalogo.repository.EditorialRepositorio;
 import catalogo.repository.LibroRepositorio;
 
-
+@Controller
 public class LibroController {
 
 
