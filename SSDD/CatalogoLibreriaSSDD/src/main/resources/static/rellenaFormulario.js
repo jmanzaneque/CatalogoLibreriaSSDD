6c7de75bdf1170afd092c179bc29b377f89f9048
jmanzaneque <jmanzaneque10@gmail.com>
2018-04-09 20:09:05 +0200

    Separado el script del HTML
    Añadido en carpeta static. Mejorada la información para el usuario al introducir el teléfono en registro y modificación de Editorial.

---
+++ SSDD/CatalogoLibreriaSSDD/src/main/resources/static/rellenaFormulario.js
@@ -0,0 +1,15 @@
+var formulario = document.getElementById("formulario"),comodin = true;
+formulario.addEventListener("submit", function(event){
+	event.preventDefault();
+	var elementos = this.elements;
+	for (var i in elementos){
+	    if (!elementos[i].value.length){
+	        alert("Complete el campo " + elementos[i].name);
+	        comodin = false;
+	        break;
+	    }
+	}
+	if (comodin){
+	    this.submit();
+	}
+	}, false);
